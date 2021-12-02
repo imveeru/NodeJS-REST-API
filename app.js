03d3a87d8b862d0065fa6841a795bd567e968921
imvee <vind.veeru@gmail.com>
2021-12-03 01:17:33 +0530

    :zap: Added GET route

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const http=require('http');
 const PORT = process.env.PORT||5000;
 
+const Todo = require("./controller");
+const { getReqData } = require("./utils");
+
 const server=http.createServer(async(req,res) => {
-    if(req.url==='/api'&&req.method=='GET'){
-        res.writeHead(200,{'Content-Type':'application/json'})
-        res.write('Hi! Fetched from API (/api)')
-        res.end()
+    if(req.url==='/api/todos'&&req.method=='GET'){
+        const todos=await Todo().getToDos()
+        res.writeHead(200, { "Content-Type": "application/json" })
+        res.end(JSON.stringify(todos))
     }
     else{
         res.writeHead(200,{'Content-Type':'application/json'})
